6ce2e54881b851739873e67884db73bae536e4d8
wangzhitao <tomsky.wang@gmail.com>
2024-08-01 21:55:59 +0800

    回退cctv-5的源

--- app/src/main/java/com/tomsky/hitv/TVViewModel.kt
+++ app/src/main/java/com/tomsky/hitv/TVViewModel.kt
@@ -21,7 +21,7 @@ class TVViewModel:ViewModel() {
 
     companion object {
         const val INVALID_INDEX = -1
-        const val VERSION = 2
+        const val VERSION = 3
     }
 
     private val tvCategoryMap = HashMap<String, TVCategoryBean>()
